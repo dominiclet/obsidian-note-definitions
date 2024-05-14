b59e04ddbbf32ad1981144f5eff0c6128dfcf4be
dominiclet <dominic.let@hotmail.com>
2024-05-14 21:53:22 +0800

    initial implementation of definition decorator

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,31 @@
 import { App, Modal, Plugin } from 'obsidian';
 import { FileParser } from './file-parser';
 import { Definition } from './model';
+import { syntaxTree } from "@codemirror/language";
+import { RangeSetBuilder } from "@codemirror/state";
+import {
+  Decoration,
+  DecorationSet,
+  EditorView,
+  PluginSpec,
+  PluginValue,
+  ViewPlugin,
+  ViewUpdate,
+  WidgetType,
+} from "@codemirror/view";
 
+let definitionCache: Map<string, Definition>;
 
 export default class NoteDefinition extends Plugin {
-	definitionCache: Map<string, Definition>;
 
 	async onload() {
-		this.definitionCache = new Map<string, Definition>();
+		definitionCache = new Map<string, Definition>();
 		const file = this.app.vault.getFileByPath("definitions/Definition.md")
 		if (file) {
 			let parser = new FileParser(this.app, file);
 			parser.parseFile().then(defs => {
 				defs.forEach(def => {
-					this.definitionCache.set(def.key, def);
+					definitionCache.set(def.key, def);
 				});
 			});
 		}
@@ -26,9 +38,11 @@ export default class NoteDefinition extends Plugin {
 				if (!curWordRange) return;
 				let curWord = editor.getRange(curWordRange.from, curWordRange.to);
 				curWord = curWord.trimStart().trimEnd().toLowerCase();
-				new DefinitionModal(this.app, curWord, this.definitionCache.get(curWord)).open();
+				new DefinitionModal(this.app, curWord, definitionCache.get(curWord)).open();
 			}
 		});
+
+		this.registerEditorExtension(viewPlugin)
 
 		// For testing purposes
 		this.addCommand({
@@ -43,6 +57,68 @@ export default class NoteDefinition extends Plugin {
 
 	}
 }
+
+class DefStylePlugin implements PluginValue {
+	decorations: DecorationSet;
+	alphabetRegex = /^[a-zA-Z]+$/;
+	terminatingChars = new Set([' ', '\n', '\r']);
+
+	constructor(view: EditorView) {
+		this.decorations = this.buildDecorations(view);
+	}
+
+	update(update: ViewUpdate) {
+		if (update.docChanged || update.viewportChanged) {
+			this.decorations = this.buildDecorations(update.view);
+		}
+	}
+
+	destroy() {}
+
+	buildDecorations(view: EditorView): DecorationSet {
+		const builder = new RangeSetBuilder<Decoration>();
+
+		for (let { from, to } of view.visibleRanges) {
+			const text = view.state.sliceDoc(from, to);
+			let wordBuf = [];
+			let word = '';
+			for (let i = 0; i < text.length; i++) {
+				let c = text.charAt(i);
+				if (wordBuf.length == 0 && this.alphabetRegex.test(c)) {
+					// start of word
+					wordBuf.push(c);
+					continue
+				}
+				if (wordBuf.length > 0 && this.terminatingChars.has(c)) {
+					word = wordBuf.join('');
+					if (definitionCache.has(word.toLowerCase())) {
+						builder.add(from + i - word.length, from + i, Decoration.mark({
+							attributes: {
+								style: 'text-decoration:underline yellow dotted',
+							}
+						}));
+					}
+					wordBuf = [];
+					word = '';
+					continue
+				}
+				if (wordBuf.length > 0) {
+					wordBuf.push(c);
+				}
+			}
+		}
+		return builder.finish();
+	}
+}
+
+const pluginSpec: PluginSpec<DefStylePlugin> = {
+	decorations: (value: DefStylePlugin) => value.decorations,
+};
+
+export const viewPlugin = ViewPlugin.fromClass(
+	DefStylePlugin,
+	pluginSpec
+);
 
 class DefinitionModal extends Modal {
 	word: string;
